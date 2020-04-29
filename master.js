2e2a2d8754ef5461ac87b0742168ce313e1376ad
hadi mozaffari tabar <mozaffaritabar.h@gmail.com>
2020-04-30 01:55:41 +0430

    Adding some graphics

--- master.js
+++ master.js
@@ -1,77 +1,107 @@
-var timer = 0;
+var speed = 300;
 var direction = 'right';
-var snake = $('#snake');
-var apple = $('#apple');
-var doc_W = $(window).width();
-var doc_H = $(window).height();
+var snake = document.getElementById('snake');
+var apple = document.getElementById('apple');
+var block = {
+    pixel: 30,
+    number: 20
+}
 
-var timerIntVal = setInterval(function () {
-    var snake_left = parseInt(snake.css('left').replace('px', ''));
-    var snake_top = parseInt(snake.css('top').replace('px', ''));
+function move_snake() {
+    var left = snake.offsetLeft;
+    var top = snake.offsetTop;
 
-    if (snake_left < -20) {
-        snake_left = doc_W + 20;
+    if (left < -block.pixel) {
+        left = block.pixel * block.number + block.number;
     } else
-    if (snake_left > doc_W + 20) {
-        snake_left = -20;
+    if (left > block.pixel * block.number) {
+        left = -block.number;
     } else
-    if (snake_top < -20) {
-        snake_top = doc_H + 20;
+    if (top < -block.pixel) {
+        top = block.number * block.pixel;
     } else
-    if (snake_top > doc_H + 20) {
-        snake_top = -20;
+    if (top > block.number * block.pixel) {
+        top = -block.pixel;
     }
 
     if (direction == 'right') {
-        snake_left += 20;
-        snake.css('left', snake_left + 'px')
+        left += block.pixel;
+        snake.style.left = left + 'px';
     } else
     if (direction == 'left') {
-        snake_left -= 20;
-        snake.css('left', snake_left + 'px')
+        left -= block.pixel;
+        snake.style.left = left + 'px';
     } else
     if (direction == 'up') {
-        snake_top -= 20;
-        snake.css('top', snake_top + 'px')
+        top -= block.pixel;
+        snake.style.top = top + 'px';
     } else
     if (direction == 'down') {
-        snake_top += 20;
-        snake.css('top', snake_top + 'px')
+        top += block.pixel;
+        snake.style.top = top + 'px';
     }
 
     if (
-        snake.css('left') === apple.css('left') &&
-        snake.css('top') === apple.css('top')
+        snake.style.left == apple.style.left &&
+        snake.style.top == apple.style.top
     ) {
         relocate_apple();
     }
 
-}, 100);
+    setTimeout(function () {
+        move_snake();
+    }, speed);
+}
 
-$(document).keydown(function (e) {
+move_snake();
+
+document.addEventListener('keydown', function (e) {
+    if (!is_snake_in_garden())
+        return false;
+
     if (e.keyCode == 38) {
-        direction = 'up';
+        set_snake_dir('up');
     } else
     if (e.keyCode == 40) {
-        direction = 'down';
+        set_snake_dir('down');
     } else
     if (e.keyCode == 37) {
-        direction = 'left';
+        set_snake_dir('left');
     } else
     if (e.keyCode == 39) {
-        direction = 'right';
+        set_snake_dir('right');
     }
 });
 
+function is_snake_in_garden() {
+    if (
+        snake.style.left < 0 ||
+        snake.style.top < 0 ||
+        snake.style.left > block.pixel * block.number ||
+        snake.style.top > block.pixel * block.number
+    ) return false;
+    return true;
+}
+
+function set_snake_dir(dir) {
+    direction = dir;
+    snake.setAttribute('dir', dir);
+}
+
 function relocate_apple() {
-    var x_portion = parseInt(doc_W / 20);
-    var y_portion = parseInt(doc_H / 20);
-    var left = parseInt(Math.random() * x_portion + 1);
-    var top = parseInt(Math.random() * y_portion + 1);
-    apple.css({
-        'left': left * 20 + 'px',
-        'top': top * 20 + 'px'
-    });
+    var left = parseInt(Math.random() * block.number - 1 + 1);
+    var top = parseInt(Math.random() * block.number - 1 + 1);
+
+    apple.style.top = top * block.pixel + 'px';
+    apple.style.left = left * block.pixel + 'px';
+
+    apple.classList.remove('shake');
+    setTimeout(function () {
+        apple.classList.add('shake');
+    }, 500);
+
+    if (speed > 100)
+        speed -= 10;
 }
 
 relocate_apple();
